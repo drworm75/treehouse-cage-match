8ce6d8564e6913b50483ce5469e80149b9b5b4e4
drworm75 <dwgames@gmail.com>
2017-04-23 21:58:29 -0500

    Has animation

--- images/Treehouse Cage Match_files/main.js
+++ images/Treehouse Cage Match_files/main.js
@@ -1,62 +1,18 @@
 $(document).ready(function() {
 
-	const player1 = [];
-	const player2 = [];
-	let winningPlayer = 0
-	let winnerBadges = [];
+	var player1 = [];
+	var player2 = [];
+
+	const animation = (winningPlayer) => {
 
 
-		let x = 0;
-
-
-		// const animation = (winningPlayer) => {
-		// 		var max = player1[winningPlayer].badges.length
-		// 		var r = Math.floor((Math.random() * max) + 1);
-		// 		console.log("loop");
-		// 		console.log(player1[winningPlayer].badges[r].icon_url)
-		// 		$('.winner-td').html('<img src="' + player1[winningPlayer].badges[r].icon_url + '" class="image1" width="25" height="25">')
-		// 	    $('.image1').animate({left: "+=300px"}, 1000);
-		// 	}
-		
-
-	const animation = () => {
-		const go = () => {
-			console.log("go", x)
-			$('.winner-td').html('<img src="' + winnerBadges[x] + '" class="image1" width="25" height="25">')
-			var r = Math.floor((Math.random() * 8) + 1);
-			console.log(r);
-		    if (r === 1) {
-			    $('.image1').animate({left: "+=300px", top: "+=300px"}, 500);
-			}
-		    if (r === 2) {
-			    $('.image1').animate({left: "+=300px", top: "-=300px"}, 500);
-			}
-		    if (r === 3) {
-			    $('.image1').animate({left: "+=300px"}, 500);
-			}
-		    if (r === 4) {
-			    $('.image1').animate({top: "+=300px"}, 500);
-		    }
-		    if (r === 5) {
-			    $('.image1').animate({left: "-=300px", top: "+=300px"}, 500);
-			}   
-		    if (r === 6) {
-			    $('.image1').animate({left: "-=300px", top: "-=300px"}, 500);
-		    }
-		    if (r === 7) {
-			    $('.image1').animate({left: "-=300px"}, 500);
-		    }
-		    if (r === 8) {
-			    $('.image1').animate({top: "-=300px"}, 500);
-		    }
-		    console.log("x", x);
-		    if (x++ < (player1[winningPlayer].badges.length - 1)) {
-		    	setTimeout(animation, 300);
-		    }
+		console.log(player1[winningPlayer].badges.length);
+		$('.form-control').val("");
+		for (var j = 0; j < 1; j++) {
+			$('.winner-td').html('<img src="' + player1[winningPlayer].badges[j].icon_url + '" class="image1" width="25" height="25">')
+	    $('.image1').animate({left: "+=300px", top: "+=300px"}, 1000);
 	    }
-		go(winningPlayer);
-		return false
-	};
+	}
 
 	$('.form-control').on('keyup', () => {
 		console.log("keyup");
@@ -93,26 +49,23 @@ $(document).ready(function() {
 				} else {
 					$("#player-two-card").html(domString);
 				}
-
+					console.log(winnerPoints)
 				if (player1[i].points.total > winnerPoints) {
-					winnerBadges = [];
 					winnerPoints = player1[i].points.total;
-					for (var j = 0; j < player1[i].badges.length; j++) {
-						winnerBadges.push(player1[i].badges[j].icon_url);						
-					}
+					console.log(winnerPoints)
 					winner = player1[i].name;
 				}
 			}
 				if (player1[0].points.total > player1[1].points.total) {
 					$('#player1-img-div').addClass('winner-td');
-					animation();
+					var winningPlayer = 0
+					animation(winningPlayer);
 				} else {
 					$('#player2-img-div').addClass('winner-td');
-					let winningPlayer = 1
-					animation();
+					var winningPlayer = 1
+					animation(winningPlayer);
 				}
 				$('.fight').addClass('winner').html(winner + " wins!<br><span>Enter two more contestants to fight again!</span>");
-				$('.form-control').val("");
 
 		}
 			
